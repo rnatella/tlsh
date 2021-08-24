c380c5b2fd4302d738c005abcaa86772bf68c508
Roberto Natella <2117196+rnatella@users.noreply.github.com>
2021-08-24 14:26:26 +0200

    C wrapper

---
+++ src/tlsh-wrapper.cpp
@@ -0,0 +1,37 @@
+#include "tlsh-wrapper.h"
+
+extern "C" Tlsh* Tlsh_new()
+{
+        return new Tlsh;
+}
+
+extern "C" const char* Tlsh_get_hash_buffer(Tlsh* tlsh, char *buffer, unsigned int bufSize, int showvers)
+{
+        return tlsh->getHash(buffer, bufSize, showvers);
+}
+
+extern "C" const char* Tlsh_get_hash(Tlsh* tlsh, int showvers)
+{
+        return tlsh->getHash(showvers);
+}
+
+extern "C" void Tlsh_update(Tlsh* tlsh, const unsigned char* data, unsigned int len)
+{
+        tlsh->update(data, len);
+}
+
+extern "C" void Tlsh_final(Tlsh* tlsh, const unsigned char* data, unsigned int len, int fc_cons_option)
+{
+        tlsh->final(data, len, fc_cons_option);
+}
+
+extern "C" int Tlsh_total_diff(Tlsh* tlsh, const Tlsh *other, int len_diff)
+{
+        return tlsh->totalDiff(other, len_diff);
+}
+
+extern "C" void Tlsh_delete(Tlsh* tlsh)
+{
+        delete tlsh;
+}
+
